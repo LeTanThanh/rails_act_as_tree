879e9b98954a248a1ad882a04315a266643dc77a
LeTanThanh <tanthanhcr95@gmail.com>
2019-02-28 16:58:04 +0700

    update_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
-rand(5).times do
+rand(10).times do
   category = Category.create(name: FFaker::Product.model, image: FFaker::Avatar.image)
 
-  rand(5).times do
+  rand(10).times do
     child_category_level_1 = category.children.create(name: FFaker::Product.model, image: FFaker::Avatar.image)
 
-    rand(5).times do
-      child_category_level_1.children.create(name: FFaker::Product.model, image: FFaker::Avatar.image)
+    rand(10).times do
+      child_category_level_2 = child_category_level_1.children.create(name: FFaker::Product.model, image: FFaker::Avatar.image)
+
+      rand(10).times do
+        child_category_level_3 = child_category_level_2.children.create(name: FFaker::Product.model, image: FFaker::Avatar.image)
+      end
     end
   end
 end
