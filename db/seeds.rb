c5e48db23ccab589d4641cfbd50f5ac2b16f3d82
LeTanThanh <tanthanhcr95@gmail.com>
2019-02-28 15:53:50 +0700

    add_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+rand(5).times do
+  category = Category.create(name: FFaker::Product.model, image: FFaker::Avatar.image)
+
+  rand(5).times do
+    child_category_level_1 = category.children.create(name: FFaker::Product.model, image: FFaker::Avatar.image)
+
+    rand(5).times do
+      child_category_level_1.children.create(name: FFaker::Product.model, image: FFaker::Avatar.image)
+    end
+  end
+end
